edc539da6ad45593233c769db494a55e5bf74dd3
dsa66253 <dsa66253@yahoo.com.tw>
2022-11-26 21:22:58 +0800

    create sample data, logIn api and classSchedule

---
+++ sample/user.js
@@ -0,0 +1,16 @@
+let users = [
+    {
+        account: "r09525126@ntu.edu.tw",
+        password: "r09525126"
+    },
+    {
+        account: "b10705032@ntu.edu.tw",
+        password: "b10705032"
+    },
+    {
+        account: "b09303042@ntu.edu.tw",
+        password: "b09303042"
+    },
+
+]
+export {users}
